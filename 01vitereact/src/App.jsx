5dca220221381cf60affcdca8f828ced9bdc9eda
Bishwash3 <bhusalbiswash2.com>
2024-10-27 13:42:34 +0545

    add message coponent in vite app

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,12 @@
+import Message from "./Message"
 
 function App() {
 
   return (
+    <>
     <h1> Hello from vite</h1>
+    <Message/>
+    </>
   )
 }
 
---
+++ 01vitereact/src/Message.jsx
@@ -0,0 +1,6 @@
+
+function Message(){
+    return <h1>Message Sent Succesfully</h1>
+}
+
+export default Message
