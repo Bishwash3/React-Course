eabcebad9514272e5037618b5bd6476a0af56282
Bishwash3 <bhusalbiswash2.com>
2024-11-06 10:55:13 +0545

    currency converter complete with custom hook

---
+++ 06currencyconverter/src/components/InputBox.jsx
@@ -0,0 +1,51 @@
+import React, { useId } from 'react';
+
+function InputBox({
+    label = "Amount",
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectCurrency = "usd",
+    amountDisable = false,
+    currencyDisable = false,
+    className = "",
+}) {
+    const amountId = useId();
+
+    return (
+        <div className={`bg-orange-400 p-3 rounded-lg text-sm flex ${className}`}>
+            <div className="w-1/2">
+                <label htmlFor={amountId} className="text-black/100 mb-2 inline-block">
+                    {label}
+                </label>
+                <input
+                    id={amountId}
+                    className="outline-none w-full bg-transparent py-1.5 bg-white text-black/100"
+                    type="number"
+                    placeholder="Amount"
+                    disabled={amountDisable}
+                    value={amount}
+                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+                />
+            </div>
+            <div className="w-1/2 flex flex-wrap justify-end text-right">
+                <p className="text-black/100 mb-2 w-full">Currency Type</p>
+                <select
+                    className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                    value={selectCurrency}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+                    disabled={currencyDisable}
+                >
+                    {currencyOptions.map((currency) => (
+                        <option key={currency} value={currency}>
+                            {currency}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        </div>
+    );
+}
+
+export {InputBox};
