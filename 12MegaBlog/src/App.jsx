bfe4e50ccb9f1b6d637a83ccb0e17a37020dbc5e
Bishwash3 <bhusalbiswash2@gmail.com>
2024-12-04 07:04:08 +0545

    add components and header, footer

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,5 +1,24 @@
+import React, {useState, useEffect} from 'react'
+import { useDispatch } from 'react-redux'
+import authService from '../appwrite/auth.js'
+import './App.css'
+import {login, logout} from './store/authSlice.js'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect( () => {
+    authService.getCurrentUser()
+    .then( (userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
   return (
     <>
       <h1>A blog app with appwrite</h1>
