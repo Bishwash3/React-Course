2fb52b1d6a1ca1c37211e0a77c88a056b0750909
Bishwash3 <bhusalbiswash2@gmail.com>
2024-12-07 06:26:32 +0545

    tailwind configure and add provider in main.js

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -3,6 +3,8 @@ import { useDispatch } from 'react-redux'
 import authService from '../appwrite/auth.js'
 import './App.css'
 import {login, logout} from './store/authSlice.js'
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -19,11 +21,18 @@ function App() {
     })
     .finally(() => setLoading(false))
   }, [])
-  return (
-    <>
-      <h1>A blog app with appwrite</h1>
-    </>
-  )
+ 
+  return !loading ? (
+    <div className='min-h-screen flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
